393746fba4c7ec5bb5ee8e57bdac7eff98f4c711
daanvdjagt <jagt0012@hz.nl>
2020-01-28 16:58:49 +0100

    fix: imports refactor: hello world

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+import Axios from "axios";
+import Vue from "vue";
+
+new Vue({
+    el: "#app",
+    data: {
+        message: "Hello world"
+    },
+    mounted() {
+        Axios.get("/skills").then(response => console.log(response.data));
+    }
+});
